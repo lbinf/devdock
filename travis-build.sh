968c1d45c1a63c96dd70adee34a7568b4707e794
lbf <290887098@qq.com>
2020-06-04 14:53:06 +0800

    init

--- travis-build.sh
+++ travis-build.sh
@@ -8,7 +8,7 @@ docker --version
 
 echo '##### Print environment'
 env | sort
-
+exit
 #### Build the Docker Images
 if [ -n "${PHP_VERSION}" ]; then
     cp env-example .env
@@ -24,22 +24,4 @@ if [ -n "${PHP_VERSION}" ]; then
     docker images
 fi
 
-#### Generate the Laradock Documentation site using Hugo
-if [ -n "${HUGO_VERSION}" ]; then
-    HUGO_PACKAGE=hugo_${HUGO_VERSION}_Linux-64bit
-    HUGO_BIN=hugo_${HUGO_VERSION}_linux_amd64
 
-    # Download hugo binary
-    curl -L https://github.com/spf13/hugo/releases/download/v$HUGO_VERSION/$HUGO_PACKAGE.tar.gz | tar xz
-    mkdir -p $HOME/bin
-    mv ./${HUGO_BIN}/${HUGO_BIN} $HOME/bin/hugo
-
-    # Remove existing docs
-    if [ -d "./docs" ]; then
-        rm -r ./docs
-    fi
-
-    # Build docs
-    cd DOCUMENTATION
-    hugo
-fi
